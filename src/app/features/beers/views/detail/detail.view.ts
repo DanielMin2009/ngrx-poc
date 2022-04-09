de2bac1dd99cbe92b028b9d17722d30d3be65b83
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-09 15:16:32 +0200

    Display beer in detail view

--- src/app/features/beers/views/detail/detail.view.ts
+++ src/app/features/beers/views/detail/detail.view.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 // Shared
 import { LayoutTypeClass } from 'src/app/shared/utils/utils';
+
+// Feature Beers
+import { BeersFacade } from '../../beers.facade';
 @Component({
   selector: 'lab-detail-view',
   templateUrl: './detail.view.html',
@@ -9,4 +13,11 @@ import { LayoutTypeClass } from 'src/app/shared/utils/utils';
 })
 export class DetailView {
   @HostBinding('class') className = LayoutTypeClass.BeersMainVerticallyTop;
+
+  constructor(public beersFacade: BeersFacade, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.beersFacade.getBeerById(id);
+  }
 }
