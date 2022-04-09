054c77b19169a57fb287cfbf5d326936b55526e0
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-09 14:29:18 +0200

    Add fetch data from API and display it to the template list with NgRX

--- src/app/features/beers/views/list/list.view.ts
+++ src/app/features/beers/views/list/list.view.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
-// RxJS
-import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 // Shared
 import { LayoutTypeClass } from 'src/app/shared/utils/utils';
 
 // Feature Beers
-import { BeerService } from '../../beers.service';
+import { BeersFacade } from '../../beers.facade';
 import { Beer } from '../../models/beer';
 @Component({
   selector: 'lab-list-view',
@@ -16,12 +15,10 @@ import { Beer } from '../../models/beer';
 export class ListView {
   @HostBinding('class') className = LayoutTypeClass.BeersMainVerticallyCentered;
 
-  beers: Observable<Beer[]> = of([]);
-
-  constructor(private beerService: BeerService) {}
+  constructor(public beersFacade: BeersFacade, private router: Router) {}
 
   ngOnInit(): void {
-    this.beers = this.beerService.getBeers();
+    this.beersFacade.getBeers();
   }
 
   getBeerById(index: number, beer: Beer): number {
