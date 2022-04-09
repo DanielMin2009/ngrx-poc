fa1e74d6d68539fb294313bd0f2d4012cb20d716
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-09 15:03:44 +0200

    Add get beer by id from API in the service

--- src/app/features/beers/beers.service.ts
+++ src/app/features/beers/beers.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+// RxJS
 import { Observable } from 'rxjs';
+
 import { Beer } from './models/beer';
+import { BeerDetails } from './models/beerDetails';
 
 const API_URL_LIST = 'https://api.punkapi.com/v2/beers';
-
+const API_URL_DETAIL = 'https://api.punkapi.com/v2/beers?ids=';
 @Injectable({
   providedIn: 'root',
 })
@@ -14,5 +16,9 @@ export class BeerService {
 
   getBeers(): Observable<Beer[]> {
     return this.http.get<Beer[]>(API_URL_LIST);
+  }
+
+  getBeerById(id: number): Observable<BeerDetails[]> {
+    return this.http.get<BeerDetails[]>(`${API_URL_DETAIL}${id}`);
   }
 }
