d22d5f62494ba6bdb1913b5add8da2714b2e7c3c
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-09 09:23:17 +0200

    Add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Views
+import { DetailView } from './features/beers/views/detail/detail.view';
+import { ListView } from './features/beers/views/list/list.view';
+
+const ROUTES: Routes = [
+  { path: 'beers', component: ListView, data: { menuItem: 1 } },
+  { path: 'beers/:id', component: DetailView, data: { menuItem: 2 } },
+
+  { path: '**', pathMatch: 'full', redirectTo: 'beers' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(ROUTES, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
