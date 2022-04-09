6d049c896e6d6f8989fb1faaa5d9ffac11388373
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-09 09:54:21 +0200

    Fetch and display data from the API
    add the right definition Beer model in the interface

--- src/app/features/beers/views/list/list.view.ts
+++ src/app/features/beers/views/list/list.view.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { BeerService } from '../../beers.service';
+import { Beer } from '../../models/beer';
 
 @Component({
   selector: 'lab-list-view',
@@ -7,4 +10,16 @@ import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 })
 export class ListView {
   @HostBinding('class') className = 'lab-view lab-view__list';
+
+  beers: Observable<Beer[]> = of([]);
+
+  constructor(private beerService: BeerService) {}
+
+  ngOnInit(): void {
+    this.beers = this.beerService.getBeers();
+  }
+
+  getBeerById(index: number, beer: Beer): number {
+    return beer.id;
+  }
 }
