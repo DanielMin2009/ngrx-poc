93da097ed60001dd60b6bcf0e1fe4d8962c5a5e2
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-10 18:58:37 +0200

    Add custom pipe to slice and remove special chars

---
+++ src/app/shared/pipes/special-chars.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  name: 'specialChars',
+})
+export class specialChars implements PipeTransform {
+  transform(value: string): string {
+    let newVal = value.replace(/[^\w\s]/gi, '').toLocaleLowerCase();
+    return this.titleCase(newVal);
+  }
+  titleCase(str: string) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+      splitStr[i] =
+        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(' ');
+  }
+}
