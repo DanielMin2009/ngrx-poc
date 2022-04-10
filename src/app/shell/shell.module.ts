c5ff867d9bd6a3cea10aa187b66d4cea6c0a5932
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-10 21:22:50 +0200

    Add search bar functionality
    Unsubscribe after subscribe to value changes to avoid memory leacks

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Routing
 import { AppRoutingModule } from 'src/app/app-routing.module';
@@ -13,16 +14,29 @@ import { ShellComponent } from './shell.component';
 // Shared
 import { LogoModule } from '../shared/components/logo/logo.module';
 import { SidenavModule } from '../shared/components/sidenav/sidenav.module';
+
+// Material
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     // Shared
     LogoModule,
     SidenavModule,
+    // Material
+    MatInputModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
+  ],
   declarations: [ShellComponent, FooterShellComponent, HeaderShellComponent],
   exports: [ShellComponent],
 })
