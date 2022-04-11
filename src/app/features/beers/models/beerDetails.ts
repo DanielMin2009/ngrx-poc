00c99d533e85e8c80d205177b7fb645dafec1088
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-11 10:44:28 +0200

    Add needed attributes

--- src/app/features/beers/models/beerDetails.ts
+++ src/app/features/beers/models/beerDetails.ts
@@ -2,7 +2,16 @@ export interface BeerDetails {
   id: number;
   name: string;
   description: string;
+  first_brewed: string;
   image_url: string;
+  abv: number;
+  ibu: number;
+  target_fg: number;
+  target_og: number;
+  ebc: number;
+  srm: number;
+  ph: number;
+  attenuation_level: number;
   ingredients: {
     malt: [
       {
