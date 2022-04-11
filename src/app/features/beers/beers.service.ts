d0d61a53004d0b4024fd3ddf517d6cef970f5718
Daniel Minguella Blasco <daniel.minguella.blasco@gmail.com>
2022-04-11 21:29:59 +0200

    Reorder service

--- src/app/features/beers/beers.service.ts
+++ src/app/features/beers/beers.service.ts
@@ -1,32 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 // RxJS
 import { Observable } from 'rxjs';
-import { ITEMS_PER_PAGE } from 'src/app/shared/definitions/units';
 
+// Shared
+import { INFINITE_SCROLL } from 'src/app/shared/definitions/units';
+
+// Definitions
 import { Beer } from './models/beer';
 import { BeerDetails } from './models/beerDetails';
 
-const API_URL_LIST = 'https://api.punkapi.com/v2/beers';
 const API_URL_DETAIL = 'https://api.punkapi.com/v2/beers?ids=';
-
-/* https://api.punkapi.com/v2/beers?page=${page}&per_page=${limit} */
-
 @Injectable({
   providedIn: 'root',
 })
 export class BeerService {
   constructor(private http: HttpClient) {}
 
-  /* getAll(page, limit = 20) {
-    return this.http.get(
-      `https://5cafa607f7850e0014629525.mockapi.io/products?page=${page}&limit=${limit}`
-    );
-  } */
-
-  getBeers({ page = 1, limit = 12 }): Observable<Beer[]> {
+  getBeers({
+    page = INFINITE_SCROLL.page,
+    limit = INFINITE_SCROLL.limit,
+  }): Observable<Beer[]> {
     return this.http.get<Beer[]>(
-      `https://api.punkapi.com/v2/beers?page=${page}&per_page=${limit}`
+      `https://api.punkapi.com/v2/brs?page=${page}&per_page=${limit}`
     );
   }
 
