94748ae683a92ac999f0f4066638e9ca2efec88a
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2024-03-28 13:55:05 +0100

    [Domain] Replace BEER to USER from the user actions

--- src/app/features/users/store/user.actions.ts
+++ src/app/features/users/store/user.actions.ts
@@ -4,48 +4,48 @@ import { UserDetails } from '../models/userDetails';
 
 enum Actions {
   // List
-  GET_BEERS = '[BEERS] Get Users',
-  GET_BEERS_SUCESS = '[BEERS] Get Users Sucess',
-  GET_BEERS_FAILURE = '[BEERS] Get Users Failure',
-  GET_BEER_BY_ID = '[BEERS] Select Users',
+  GET_USERS = '[USERS] Get Users',
+  GET_USERS_SUCESS = '[USERS] Get Users Sucess',
+  GET_USERS_FAILURE = '[USERS] Get Users Failure',
+  GET_USER_BY_ID = '[USERS] Select Users',
   // Details
-  GET_BEER_DETAILS = '[BEER] Get User Details',
-  GET_BEER_DETAILS_SUCCESS = '[BEER] Get User Details Success',
-  GET_BEER_DETAILS_FAILURE = '[BEER] Get User Details Failure',
+  GET_USER_DETAILS = '[USER] Get User Details',
+  GET_USER_DETAILS_SUCCESS = '[USER] Get User Details Success',
+  GET_USER_DETAILS_FAILURE = '[USER] Get User Details Failure',
   // Filter
-  SET_FILTER_QUERY = '[BEER] Set Filter By Name',
-  RESET_QUERY = '[BEER] Reset Query',
+  SET_FILTER_QUERY = '[USER] Set Filter By Name',
+  RESET_QUERY = '[USER] Reset Query',
 }
 
 export const getUsers = createAction(
-  Actions.GET_BEERS,
+  Actions.GET_USERS,
   props<{ filter: { page: number; limit: number } }>()
 );
 
 export const getUsersSucess = createAction(
-  Actions.GET_BEERS_SUCESS,
+  Actions.GET_USERS_SUCESS,
   props<{ users: User[] }>()
 );
 
 export const getUsersFailure = createAction(
-  Actions.GET_BEERS_FAILURE,
+  Actions.GET_USERS_FAILURE,
   props<{ error: any }>()
 );
 
 export const getUserById = createAction(
-  Actions.GET_BEER_BY_ID,
+  Actions.GET_USER_BY_ID,
   props<{ userId: number }>()
 );
 
-export const getUserDetails = createAction(Actions.GET_BEER_DETAILS);
+export const getUserDetails = createAction(Actions.GET_USER_DETAILS);
 
 export const getUserDetailsSucess = createAction(
-  Actions.GET_BEER_DETAILS_SUCCESS,
+  Actions.GET_USER_DETAILS_SUCCESS,
   props<{ userDetails: UserDetails }>()
 );
 
 export const getUserDetailsFailure = createAction(
-  Actions.GET_BEER_DETAILS_FAILURE,
+  Actions.GET_USER_DETAILS_FAILURE,
   props<{ error: any }>()
 );
 
